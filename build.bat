cc8359cdda05e4d5c09540769b47801abc7a5c7d
brinkqiang <brink.qiang@gmail.com>
2024-07-08 11:47:38 +0800

    add

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
+
 rmdir /S /Q build
 mkdir build
-cd build
+pushd build
+
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cd ..
+cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
+popd
+
+rem pause
--- build.sh
+++ build.sh
@@ -1,26 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 # - install depends tools
 # yum -y install git
 # yum -y install gcc gcc-c++ autoconf libtool automake make
 #
 
-# - clone code
-# git clone https://github.com/brinkqiang/dmlogger.git
-# pushd dmlogger
-# git submodule update --init --recursive
-#
-
-# pushd depends_path
-libtoolize && aclocal && autoheader && autoconf && automake --add-missing
-sh configure
+# pushd thirdparty/depends_path
+# libtoolize && aclocal && autoheader && autoconf && automake --add-missing
+# sh configure
 # popd
 
 rm -rf build
-mkdir build
+mkdir -p build
 pushd build
+
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-make -j1
+cmake --build . --config relwithdebinfo -- -j$(nproc)
+
 popd
 
 # popd
+
+# echo continue && read -n 1
